fea823855e18e34e2942ae9c00bf075abbbf232e
Option0417 <option0417@gmail.com>
2012-01-14 19:40:38 +0800

    imoort to Eclipse

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
  *  Created on: 2011/11/6
  *      Author: option0417
  */
-#include "DataStructure\Stack\IntStack.h"
-#include "Algorithms\InsertionSort\InsertSort.h"
-#include "Algorithms\SelectionSort\SelectionSort.h"
-#include "Algorithms\BubbleSort\BubbleSort.h"
+#include "DataStructure/Stack/IntStack.h"
+#include "Algorithms/InsertionSort/InsertSort.h"
+#include "Algorithms/SelectionSort/SelectionSort.h"
+#include "Algorithms/BubbleSort/BubbleSort.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -16,36 +16,36 @@ void single99();
 void double99();
 
 int main(void) {
-//	srand(time(NULL));
-//	int size = 0;
-//	std::cin>>size;
-//
-//	int vals[size];
-//
-//	for (int i = 0; i < size; i++) {
-//		vals[i] = (int)rand() % 100 + 1;
-//	}
-//
-//	for (int i = 0; i < size; i++) {
-//		std::cout<<vals[i]<<" ";
-//	}
-//	std::cout<<std::endl;
-//
-//	//InsertSort* sort = new InsertSort(vals, 6);
-//	//IAlgorithm* sort = new SelectionSort(vals, 7);
-//	IAlgorithm* sort = new BubbleSort(vals, size);
-//
-//	sort->sort();
-//
-//	for (int i = 0; i < size; i++) {
-//		std::cout<<vals[i]<<" ";
-//	}
-//	std::cout<<std::endl;
+	srand(time(NULL));
+	int size = 0;
+	std::cin>>size;
+
+	int vals[size];
+
+	for (int i = 0; i < size; i++) {
+		vals[i] = (int)rand() % 100 + 1;
+	}
+
+	for (int i = 0; i < size; i++) {
+		std::cout<<vals[i]<<" ";
+	}
+	std::cout<<std::endl;
+
+	//InsertSort* sort = new InsertSort(vals, 6);
+	//IAlgorithm* sort = new SelectionSort(vals, 7);
+	IAlgorithm* sort = new BubbleSort(vals, size);
+
+	sort->sort();
+
+	for (int i = 0; i < size; i++) {
+		std::cout<<vals[i]<<" ";
+	}
+	std::cout<<std::endl;
 
 	//sort->show();
 
 	//single99();
-	double99();
+	//double99();
 
 	return 0;
 }
